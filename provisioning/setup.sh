e6016eaee1db2b57c60b97bba97c87a44c0e12bb
Stephan Hohn <stephanhohn@gmail.com>
2017-07-15 10:04:19 +0200

    Get ibsim and fabricmon running in tmux session

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -7,27 +7,33 @@ echo "Update and Upgrade"
 apt-get update
 apt-get upgrade -y
 
-echo "Install required pkgs"
-apt-get install -y nginx ibsim-utils
-apt-get install -y libopensm5a libibumad3a libibnetdisc5 libopensm5a
-
-apt-get install -y golang
+echo "Install golang and othre dev requirements"
+add-apt-repository ppa:longsleep/golang-backports
+apt-get update
+apt-get install -y golang-go
 apt-get install -y libibmad-dev libibumad-dev libibnetdisc-dev libopensm-dev
+apt-get install -y libopensm5a libibumad3 libibnetdisc5 libopensm5a
 
+echo "Install required pkgs"
+apt-get install -y nginx
+apt-get install -y ibsim-utils
 apt-get install -y influxdb influxdb-client
 apt-get install -y grafana grafana-data
-
-echo "Doing go stuff"
-export GOPATH=/usr/lib/go/
-go get -u github.com/influxdata/influxdb/client/v2
-go get -u github.com/iansmith/d3
 
 echo "edit configs"
 sed -i 's/root \/var\/www\/html\;/root \/vagrant\/webui\;/g' /etc/nginx/sites-available/default
 sed -i 's/ubuntu-xenial/fabricmon/g' /etc/hosts
 
+echo "Doing go stuff"
+export GOPATH=/home/ubuntu/go/
+sudo -u ubuntu -H go get -u github.com/BurntSushi/toml
+sudo -u ubuntu -H go get -u github.com/influxdata/influxdb/client/v2
+
 echo "Enable and restart nginx"
 systemctl enable nginx.service
 systemctl restart nginx.service
+
+echo "Start ibsim"
+sudo -u ubuntu -H tmux new -d -s ibsim_and_fabricmon_daemon 'cd /vagrant; LD_PRELOAD=/usr/lib/umad2sim/libumad2sim.so go run *.go; read' \; split-window -d 'cd /vagrant; sudo ibsim -s ibsim.net; read' \;
 
 echo "You can now reach fabricmon webUI via http://localhost:8080"
